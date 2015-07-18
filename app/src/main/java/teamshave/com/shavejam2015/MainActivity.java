2c3f3112d8b2a0e3893c29ab746591632bf175f0
Dave Tansley <dave.tansley@gmail.com>
2015-07-18 15:27:26 +0100

    Added extra field for tip percent, tightened up outputs

--- app/src/main/java/teamshave/com/shavejam2015/MainActivity.java
+++ app/src/main/java/teamshave/com/shavejam2015/MainActivity.java
@@ -1,7 +1,5 @@
 package teamshave.com.shavejam2015;
 
-import android.content.Intent;
-import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.text.TextUtils;
@@ -12,12 +10,15 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     protected Button btn;
     protected TextView msg;
     protected EditText bill;
+    protected EditText tipPercent;
 
 
     @Override
@@ -25,26 +26,49 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        btn=(Button)findViewById(R.id.button);
-        bill=(EditText)findViewById(R.id.editText);
+        btn=(Button)findViewById(R.id.calculateButton);
+        bill=(EditText)findViewById(R.id.billAmountEditText);
         msg=(TextView)findViewById(R.id.textView2);
+        tipPercent=(EditText)findViewById(R.id.tipAmountEditText);
 
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
                 String billText = bill.getText().toString();
+                String tipPercentText = tipPercent.getText().toString();
                 double tip;
                 double billAmount;
+                int tipPercent;
 
+                // Calculate the tip percent
+                if (!TextUtils.isEmpty(tipPercentText)){
+                    try {
+                        tipPercent = Integer.parseInt(tipPercentText);
+
+                        if (tipPercent < 0 || tipPercent > 100) {
+                            throw new NumberFormatException(getString(R.string.exception_tip_percent_outside_range));
+                        }
+
+                    } catch (NumberFormatException err) {
+                        msg.setText(getString(R.string.error_invalid_tip_percent));
+                        return;
+                    }
+
+                } else {
+                    msg.setText(getString(R.string.error_invalid_tip_percent));
+                    return;
+                }
+
+                // Calculate the bill amount
                 if (!TextUtils.isEmpty(billText)) {
                     try {
                         billAmount = Double.parseDouble(bill.getText().toString());
 
-                        int tipPercent = 15;
-                        tip = Calculations.CalculateTip(billAmount, tipPercent);
 
-                        msg.setText(String.valueOf(tip));
+                        tip = Calculations.CalculateTip(billAmount, tipPercent);
+                        DecimalFormat df = new DecimalFormat("#.00");
+                        msg.setText(df.format(tip));
 
                     } catch (NumberFormatException err) {
                         msg.setText(getString(R.string.error_invalid_bill_amount));
@@ -54,10 +78,6 @@ public class MainActivity extends AppCompatActivity {
                 } else {
                     msg.setText(getString(R.string.error_no_bill_amount));
                 }
-
-
-
-
 
             }
         });
